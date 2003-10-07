1e5902c9b2416f6866bb138acacbf4ff2ff9988c
Lars J. Aas <larsa@sim.no>
2003-10-07 14:49:14 +0000

    empty shell for scenery view controller

---
+++ lib/SmallChange/nodes/SmSceneryView.cpp
@@ -0,0 +1,32 @@
+
+#include <SmallChange/nodes/SmSceneryView.h>
+
+SO_NODE_SOURCE(SmSceneryView);
+
+void
+SmSceneryView::initClass(void)
+{
+}
+
+SmSceneryView::SmSceneryView(void)
+{
+  SO_NODE_CONSTRUCTOR(SmSceneryView);
+
+#if 0
+  SoSFInt32 viewId;
+  SoMFVec3f hotspots;
+  SoSFBool hotCamera;
+
+  SoSFFloat blockRottger;
+  SoSFFloat loadRottger;
+
+  SoMFInt32 renderSequence;
+
+  SoSFBool visualDebug;
+#endif
+}
+
+SmSceneryView::~SmSceneryView(void)
+{
+}
+
