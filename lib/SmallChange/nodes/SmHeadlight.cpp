52a7576b7331abc874626e5150a7ff97eca5955b
Peder Blekken <pederb@sim.no>
2003-10-22 09:02:42 +0000

    Headlight node implementation.

--- lib/SmallChange/nodes/SmHeadlight.cpp
+++ lib/SmallChange/nodes/SmHeadlight.cpp
@@ -1,1 +1,123 @@
-// coming soon
+/**************************************************************************\
+ *
+ *  This file is part of the SmallChange extension library for Coin.
+ *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  ("GPL") version 2 as published by the Free Software Foundation.
+ *  See the file LICENSE.GPL at the root directory of this source
+ *  distribution for additional information about the GNU GPL.
+ *
+ *  For using SmallChange with software that can not be combined with the
+ *  GNU GPL, and for taking advantage of the additional benefits of our
+ *  support services, please contact Systems in Motion about acquiring
+ *  a Coin Professional Edition License.
+ *
+ *  See <URL:http://www.coin3d.org> for  more information.
+ *
+ *  Systems in Motion, Teknobyen, Abels Gate 5, 7030 Trondheim, NORWAY.
+ *  <URL:http://www.sim.no>.
+ *
+\**************************************************************************/
+
+/*!
+  \class SmHeadlight SmHeadlight.h SmallChange/nodes/SmHeadlight.h
+  \brief The SmHeadlight class is a node type for specifying a viewer headlight.
+  \ingroup nodes
+
+  See also documentation of parent class for important information
+  regarding light sources in general.
+*/
+
+#include <SmallChange/nodes/SmHeadlight.h>
+
+#include <Inventor/SbColor4f.h>
+#include <Inventor/SbVec4f.h>
+#include <Inventor/actions/SoGLRenderAction.h>
+#include <Inventor/elements/SoEnvironmentElement.h>
+#include <Inventor/elements/SoGLLightIdElement.h>
+#include <Inventor/elements/SoViewVolumeElement.h>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif // HAVE_CONFIG_H
+#include <Inventor/system/gl.h>
+
+#if COIN_DEBUG
+#include <Inventor/errors/SoDebugError.h>
+#endif // COIN_DEBUG
+
+// *************************************************************************
+
+SO_NODE_SOURCE(SmHeadlight);
+
+/*!
+  Constructor.
+*/
+SmHeadlight::SmHeadlight(void)
+{
+  SO_NODE_CONSTRUCTOR(SmHeadlight);
+}
+
+/*!
+  Destructor.
+*/
+SmHeadlight::~SmHeadlight()
+{
+}
+
+// Doc from superclass.
+void
+SmHeadlight::initClass(void)
+{
+  static int initialized = 0;
+  if (!initialized) {
+    initialized = 1;
+    SO_NODE_INIT_CLASS(SmHeadlight, SoLight, "Light");
+  }
+}
+
+// Doc from superclass.
+void
+SmHeadlight::GLRender(SoGLRenderAction * action)
+{
+  if (!this->on.getValue()) return;
+
+  SoState * state = action->getState();
+  int idx = SoGLLightIdElement::increment(state);
+
+  if (idx < 0) {
+#if COIN_DEBUG
+    SoDebugError::postWarning("SmHeadlight::GLRender",
+                              "Max # of OpenGL lights exceeded :(");
+#endif // COIN_DEBUG
+    return;
+  }
+
+  GLenum light = (GLenum) (idx + GL_LIGHT0);
+  
+  SbColor4f lightcolor(0.0f, 0.0f, 0.0f, 1.0f);
+  // disable ambient contribution from this light source
+  glLightfv(light, GL_AMBIENT, lightcolor.getValue()); 
+  
+  lightcolor.setRGB(this->color.getValue());
+  lightcolor *= this->intensity.getValue();
+
+  glLightfv(light, GL_DIFFUSE, lightcolor.getValue());
+  glLightfv(light, GL_SPECULAR, lightcolor.getValue());
+
+  // GL directional light is specified towards light source
+  SbVec3f dir = - SoViewVolumeElement::get(state).getProjectionDirection();
+  dir.normalize();
+
+  // directional when w = 0.0
+  SbVec4f dirvec(dir[0], dir[1], dir[2], 0.0f);
+  glLightfv(light, GL_POSITION, dirvec.getValue());
+
+  glLightf(light, GL_SPOT_EXPONENT, 0.0);
+  glLightf(light, GL_SPOT_CUTOFF, 180.0);
+  glLightf(light, GL_CONSTANT_ATTENUATION, 1);
+  glLightf(light, GL_LINEAR_ATTENUATION, 0);
+  glLightf(light, GL_QUADRATIC_ATTENUATION, 0);
+}
