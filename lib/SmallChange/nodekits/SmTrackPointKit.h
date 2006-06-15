ac681eeeee80a063bc1cff735ed8f621fc869dd1
P?l-Robert Engn?s <preng@sim.no>
2006-06-15 07:27:06 +0000

    Typo in copyright notice.

--- lib/SmallChange/nodekits/SmTrackPointKit.h
+++ lib/SmallChange/nodekits/SmTrackPointKit.h
@@ -4,7 +4,7 @@
 /**************************************************************************\
  *
  *  This file is part of the SmallChange extension library for Coin.
- *  Copyright (C) 1998-2003 by Systems in Motion.  All rights reserved.
+ *  Copyright (C) 1998-2006 by Systems in Motion.  All rights reserved.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
