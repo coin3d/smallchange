7a4cade09f00dad35a650d593442b06653d7fcb5
Lars J. Aas <larsa@sim.no>
2006-09-13 18:42:41 +0000

    upgrade to current scheme

--- build/msvc7/generate.sh
+++ build/msvc7/generate.sh
@@ -1,26 +1,54 @@
 #! /bin/sh
 #
 # This script generates the Visual Studio 6 build files for Windows.
-# The MSVC7 conversion is currently done manually.
 #
 # 20041214 larsa
 
-rm -rf smallchange1.dsp smallchange1.dsw installsmallchangeheaders.bat
-
-../../configure --enable-msvcdsp --with-msvcrt=mt
-make
+rm -f smallchange1.dsp smallchange1.dsw smallchange1.vcproj smallchange1.sln install-headers.bat
 
 build_pwd=`pwd`
 build="`cygpath -w $build_pwd | sed -e 's/\\\\/\\\\\\\\/g'`"
+build_pwd="`pwd | sed -e 's/\\//\\\\\\\\/g'`\\\\"
+
 source_pwd=`cd ../..; pwd`
 source="`cygpath -w $source_pwd | sed -e 's/\\\\/\\\\\\\\/g'`"
+source_pwd="`(cd ../..; pwd) | sed -e 's/\\//\\\\\\\\/g'`"
+
+../../configure --with-msvcrt=mtd --with-suffix=d \
+  --enable-debug --enable-symbols || exit 1
+cp config.h lib/config-debug.h
+
+../../configure --enable-msvcdsp --with-msvcrt=mt \
+  --disable-debug --disable-symbols --enable-optimization || exit 1
+cp config.h lib/config-release.h
+
+cp config-wrapper.h lib/config.h
+
+make || exit 1
 
 sed \
   -e "s/$build/./g" \
+  -e "s/$build_pwd//g" \
   -e "s/$source/..\\\\../g" \
+  -e "s/$source_pwd/..\\\\../g" \
   -e 's/$/\r/g' \
   <smallchange1.dsp >new.dsp
+
 mv new.dsp smallchange1.dsp
+
+sed \
+  -e "s/$build/./g" \
+  -e "s/$build_pwd//g" \
+  -e "s/$source/..\\\\../g" \
+  -e "s/$source_pwd/..\\\\../g" \
+  -e 's/$/\r/g' \
+  <install-headers.bat >new.bat
+
+mv new.bat install-headers.bat
+
+echo "Now upgrade the project files to MSVC7.1..."
+echo "Hopefully you remembered to generate in msvc6 first."
+echo "Do: devenv smallchange1.dsw"
 
 # How can I avoid the modal upgrade prompt-dialog for MSVC7.1 here???
 # devenv /command "File.OpenProject $build\\smallchange1.dsp"
