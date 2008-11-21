59933886b145ea28c2d106fb777fcee1a7cd4905
Lars Moastuen <larsm@sim.no>
2008-11-21 16:35:03 +0000

    MSVC compilefix.

--- lib/SmallChange/nodes/SmTextureText2Collector.cpp
+++ lib/SmallChange/nodes/SmTextureText2Collector.cpp
@@ -88,18 +88,18 @@ SmTextureText2Collector::SmTextureText2Collector(void)
 SmTextureText2Collector::~SmTextureText2Collector()
 {
 }
-  
-void 
+
+void
 SmTextureText2Collector::initClass(void)
 {
   SO_NODE_INIT_CLASS(SmTextureText2Collector, SoSeparator, "Separator");
   SO_ENABLE(SoGLRenderAction, SmTextureText2CollectorElement);
 }
-  
-void 
+
+void
 SmTextureText2Collector::GLRenderInPath(SoGLRenderAction * action)
 {
-  SoState * state = action->getState();  
+  SoState * state = action->getState();
   SmTextureText2CollectorElement::startCollecting(state, false);
   inherited::GLRenderInPath(action);
   (void) SmTextureText2CollectorElement::finishCollecting(state);
@@ -127,27 +127,27 @@ static SbBool get_screenpoint_pixels(const SbVec3f & screenpoint,
   return FALSE;
 }
 
-void 
+void
 SmTextureText2Collector::GLRenderBelowPath(SoGLRenderAction * action)
 {
-  SoState * state = action->getState();  
+  SoState * state = action->getState();
   SoCacheElement::invalidate(state);
-  
+
   // for Coin-3
-  // bool transppass = action->isRenderingTranspPaths(); 
+  // bool transppass = action->isRenderingTranspPaths();
 
   bool transppass = action->handleTransparency(FALSE);
-  
+
   SmTextureText2CollectorElement::startCollecting(state, !transppass);
   inherited::GLRenderBelowPath(action);
 
-  const std::vector<SmTextureText2CollectorElement::TextItem> & items = 
+  const std::vector<SmTextureText2CollectorElement::TextItem> & items =
     SmTextureText2CollectorElement::finishCollecting(state);
 
   if (!transppass && items.size()) {
     // FIXME: sort items based on font
     state->push();
-    
+
     SbMatrix normalize(0.5f, 0.0f, 0.0f, 0.0f,
                        0.0f, 0.5f, 0.0f, 0.0f,
                        0.0f, 0.0f, 1.0f, 0.0f,
@@ -160,7 +160,7 @@ SmTextureText2Collector::GLRenderBelowPath(SoGLRenderAction * action)
     const SbViewVolume & vv = SoViewVolumeElement::get(state);
     const SbViewportRegion & vp = SoViewportRegionElement::get(state);
     const SbVec2s vpsize = vp.getViewportSizePixels();
-    
+
     // Set up new view volume
     glMatrixMode(GL_MODELVIEW);
     glPushMatrix();
@@ -170,9 +170,9 @@ SmTextureText2Collector::GLRenderBelowPath(SoGLRenderAction * action)
     glLoadIdentity();
     glOrtho(0, vpsize[0], 0, vpsize[1], -1.0f, 1.0f);
 
-    // set up texture and rendering 
+    // set up texture and rendering
     const SmTextureFont::FontImage * currentfont = items[0].font;
-    SoLightModelElement::set(state, SoLightModelElement::BASE_COLOR); 
+    SoLightModelElement::set(state, SoLightModelElement::BASE_COLOR);
     SoTextureQualityElement::set(state, 0.3f);
     SoGLTextureImageElement::set(state, this,
                                  currentfont->getGLImage(),
@@ -197,22 +197,22 @@ SmTextureText2Collector::GLRenderBelowPath(SoGLRenderAction * action)
       float dist = -nearplane.getDistance(items[i].worldpos);
       if ((dist < 0.0f) ||
           ((items[i].maxdist > 0.0f) && (dist > items[i].maxdist))) continue;
-      
+
       glColor4fv(items[i].color.getValue());
       int len = items[i].text.getLength();
       if (len == 0) continue;
 
       const unsigned char * sptr =
         reinterpret_cast<const unsigned char *>(items[i].text.getString());
-      
+
       if (items[i].font != currentfont) {
         glEnd();
         currentfont = items[i].font;
         SoGLTextureImageElement::set(state, this,
                                      currentfont->getGLImage(),
                                      SoTextureImageElement::MODULATE,
-                                     SbColor(1.0f, 1.0f, 1.0f));        
-        SoGLLazyElement::getInstance(state)->send(state, 
+                                     SbColor(1.0f, 1.0f, 1.0f));
+        SoGLLazyElement::getInstance(state)->send(state,
                                                   SoLazyElement::GLIMAGE_MASK);
 
         glBegin(GL_QUADS);
@@ -221,7 +221,7 @@ SmTextureText2Collector::GLRenderBelowPath(SoGLRenderAction * action)
       projmatrix.multVecMatrix(items[i].worldpos, screenpoint);
 
       short ymin = short(-currentfont->getLeading());
-      
+
       switch (items[i].vjustification) {
       case SmTextureText2::BOTTOM:
         break;
@@ -235,14 +235,14 @@ SmTextureText2Collector::GLRenderBelowPath(SoGLRenderAction * action)
         assert(0 && "unknown alignment");
         break;
       }
-      short w = (short) currentfont->stringWidth(items[i].text); 
-    
+      short w = (short) currentfont->stringWidth(items[i].text);
+
       SbVec2s sp;
       if (!get_screenpoint_pixels(screenpoint, vpsize, sp)) continue;
 
       SbVec2s n0 = SbVec2s(sp[0],
                            sp[1] + ymin);
-      
+
       switch (items[i].justification) {
       case SmTextureText2::LEFT:
         break;
@@ -260,9 +260,9 @@ SmTextureText2Collector::GLRenderBelowPath(SoGLRenderAction * action)
     }
     glEnd();
     glPopAttrib();
-    SoGLLazyElement::getInstance(state)->reset(state, 
+    SoGLLazyElement::getInstance(state)->reset(state,
                                                SoLazyElement::DIFFUSE_MASK);
-    
+
     glPopMatrix();
     glMatrixMode(GL_MODELVIEW);
     glPopMatrix();
@@ -275,7 +275,7 @@ SmTextureText2Collector::GLRenderBelowPath(SoGLRenderAction * action)
 
 SO_ELEMENT_SOURCE(SmTextureText2CollectorElement);
 
-void 
+void
 SmTextureText2CollectorElement::initClass(void)
 {
   SO_ELEMENT_INIT_CLASS(SmTextureText2CollectorElement, inherited);
@@ -284,30 +284,30 @@ SmTextureText2CollectorElement::initClass(void)
 SmTextureText2CollectorElement::~SmTextureText2CollectorElement()
 {
 }
- 
-void 
+
+void
 SmTextureText2CollectorElement::init(SoState * state)
 {
   this->collecting = false;
   this->storeitems = false;
 }
 
-void 
+void
 SmTextureText2CollectorElement::startCollecting(SoState * state, const bool storeitems)
 {
-  SmTextureText2CollectorElement * elem = 
-    static_cast<SmTextureText2CollectorElement*> 
+  SmTextureText2CollectorElement * elem =
+    static_cast<SmTextureText2CollectorElement*>
     (state->getElementNoPush(classStackIndex));
   elem->items.clear();
   elem->collecting = true;
   elem->storeitems = storeitems;
 }
- 
-bool 
+
+bool
 SmTextureText2CollectorElement::isCollecting(SoState * state)
 {
-  SmTextureText2CollectorElement * elem = 
-    static_cast<SmTextureText2CollectorElement*> 
+  SmTextureText2CollectorElement * elem =
+    static_cast<SmTextureText2CollectorElement*>
     (state->getElementNoPush(classStackIndex));
   return elem->collecting;
 }
@@ -322,10 +322,10 @@ SmTextureText2CollectorElement::add(SoState * state,
 				    SmTextureText2::Justification j,
 				    SmTextureText2::VerticalJustification vj)
 {
-  SmTextureText2CollectorElement * elem = 
-    static_cast<SmTextureText2CollectorElement*> 
+  SmTextureText2CollectorElement * elem =
+    static_cast<SmTextureText2CollectorElement*>
     (state->getElementNoPush(classStackIndex));
-  
+
   assert(elem->collecting);
   if (!elem->storeitems) return;
 
@@ -341,11 +341,11 @@ SmTextureText2CollectorElement::add(SoState * state,
   elem->items.push_back(item);
 }
 
-const std::vector <SmTextureText2CollectorElement::TextItem> & 
+const std::vector <SmTextureText2CollectorElement::TextItem> &
 SmTextureText2CollectorElement::finishCollecting(SoState * state)
 {
-  SmTextureText2CollectorElement * elem = 
-    static_cast<SmTextureText2CollectorElement*> 
+  SmTextureText2CollectorElement * elem =
+    static_cast<SmTextureText2CollectorElement*>
     (state->getElementNoPush(classStackIndex));
   elem->collecting = false;
   elem->storeitems = false;
@@ -356,6 +356,7 @@ SbBool
 SmTextureText2CollectorElement::matches(const SoElement * elt) const
 {
   assert(0 && "should never be called");
+  return false;
 }
 
 SoElement *
