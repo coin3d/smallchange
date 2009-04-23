0c7eaa2a1bcfcf47d153b97dc3ad4c3483b4cf5f
Tom Fredrik Blenning Klaussen <bfg@sim.no>
2009-04-23 07:44:46 +0000

    DynamicBaseKit, SoBaseKit instance of DynamicNodeKit

---
+++ lib/SmallChange/nodekits/DynamicBaseKit.h
@@ -0,0 +1,9 @@
+#ifndef DynamicBaseKit_H
+#define DynamicBaseKit_H
+
+#include "DynamicNodeKit.h"
+
+class SoBaseKit;
+typedef DynamicNodeKit<SoBaseKit> DynamicBaseKit;
+
+#endif // DynamicBaseKit_H
